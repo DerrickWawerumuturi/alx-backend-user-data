c260edf7876029347c862a05c61507e06be356a3
DerrickWawerumuturi <wawerumuturi57@gmail.com>
2024-09-09 14:27:34 +0300

    auth

--- 0x01-Basic_authentication/api/v1/auth/basic_auth.py
+++ 0x01-Basic_authentication/api/v1/auth/basic_auth.py
@@ -21,3 +21,11 @@ class BasicAuth(Auth):
         if not authorization_header.startswith('Basic '):
             return None
         return authorization_header[len('Basic '):]
+
+    def decode_base64_authorization_header(
+        self,
+        base64_authorization_header: str
+            )-> str:
+        """ returns the decoded valueof Base64 str
+        """
+        
